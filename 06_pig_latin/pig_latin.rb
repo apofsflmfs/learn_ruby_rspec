bc7f1482bfadd428f0ece3e6a3815d5f46cf5013
ssimonlp <simon.lepavec@gmail.com>
2018-10-08 16:33:07 +0200

    first version pig latin

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
 #write your code here
+def translate (string)
+	result = []
+	array = string.split (" ")
+	array.each do |x|
+		if x.start_with?("a","e","i","o","u","y")
+			result << x + "ay"
+		else
+			n = x.split(/[aeiouy](.*)/)[0].length
+			x << x.split(/[aeiouy](.*)/)[0]
+			result <<  x[n..-1] + "ay"
+		end
+	end
+	return result.join(" ")
+end
