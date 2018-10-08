ced32fffccc3c9c0cd8bc932b4bd8c963ee849b6
ssimonlp <simon.lepavec@gmail.com>
2018-10-08 11:27:40 +0200

    final version calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
-#write your code here
+#write code here
+def add (n, m)
+	sum = [n, m].reduce :+
+	return sum
+end
+
+def subtract (n, m)
+	diff = [n, m].reduce :-
+	return diff
+end
+
+def sum (arr)
+	if arr == []
+		return 0
+	else
+		return arr.reduce(&:+)
+	end
+end
+
+def multiply (n, m)
+	return [n, m].reduce :*
+end
+
+def power (n, m)
+	return n**m
+end
+
+def factorial (n)
+	if n >= 1
+		return (1..n).reduce :*
+	else
+		return 0
+	end
+end
