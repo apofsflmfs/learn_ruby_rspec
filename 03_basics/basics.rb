3cd19c88f43c9ba91a1eb5f49618f898ba0a37ce
Crysicia <lucienmontmartre@yahoo.fr>
2018-10-08 11:38:59 +0200

    Added basicis.rb with bonus

--- 03_basics/basics.rb
+++ 03_basics/basics.rb
@@ -1,1 +1,20 @@
-# write your code here
+def who_is_bigger(a, b, c)
+    arr = [a, b, c]
+    if arr.any?{|n| n.nil?}
+        return "nil detected"
+    else
+        return "#{arr.index(arr.max) == 0 ? "a is bigger" : arr.index(arr.max) == 1 ? "b is bigger" : "c is bigger"}"
+    end
+end
+
+def reverse_upcase_noLTA(str)
+    return str.upcase.chars.reverse.join.tr("LTA", "")
+end
+
+def array_42(arr)
+    return arr.count(42) > 0
+end
+
+def magic_array(arr)
+    arr.flatten.sort.map{|x|x*2}.delete_if{|n|n%3==0}.uniq
+end
